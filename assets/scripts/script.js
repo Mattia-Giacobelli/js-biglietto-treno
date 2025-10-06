f772848c22edef33d6ef956846ee3bec5172f98b
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-06 15:31:55 +0200

    Create Human code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,19 @@
 .console.log('It Works');
+
+
+/*Richiesta dati utente */
+
+//Richieta età
+
+//Richiesta Distanza da percorrere
+
+
+/*Calcolo del prezzo del viaggio*/
+
+//Calcolo del prezzo di base (0.21 € al km)
+
+//Applicazione sconto minorenni
+
+//Applicazione sconto over 65
+
+//Output prezzo finale in forma umana
