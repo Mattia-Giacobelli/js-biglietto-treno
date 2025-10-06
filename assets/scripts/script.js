21339bee67a98955cff53ff8906512f85368d882
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-06 15:38:29 +0200

    create richiesta dati utente

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,15 @@
-.console.log('It Works');
+console.log('It Works');
 
 
 /*Richiesta dati utente */
 
 //Richieta età
 
+const eta = Number(prompt("Inserisci l'età"))
+
 //Richiesta Distanza da percorrere
 
+const distanza = Number(prompt('Inserire i Km di viaggio'))
 
 /*Calcolo del prezzo del viaggio*/
 
